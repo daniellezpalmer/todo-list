f7e1c06ce0a64d58d305f186e661d47cd8572b21
Danielle Palmer <daniellezpalmer@gmail.com>
2017-08-22 13:30:23 -0500

    Restructured files

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const expressValidator = require('express-validator');
 const mustache = require('mustache');
 const mustacheExpress = require('mustache-express');
+const data = require('./models/todoData.js')
 const app = express();
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,60 +17,8 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 app.get('/', function(req, res) {
-  res.render('todo.mustache', {items:theList});
+  res.render('todo.mustache', {items:data.list});
 })
-
-let theList = [{
-    'text': "Learn Node basics",
-    'done': true,
-    'id': 1
-  },
-  {
-    'text': "Learn Express basics",
-    'done': true,
-    'id': 2
-  },
-  {
-    'text': "Learn Mustache",
-    'done': true,
-    'id': 3
-  },
-  {
-    'text': "Learn HTML forms with Express",
-    'done': false,
-    'id': 4
-  },
-  {
-    'text': "Learn about authentication",
-    'done': false,
-    'id': 5
-  },
-  {
-    'text': "Learn how to connect to PostgreSQL",
-    'done': false,
-    'id': 6
-  },
-  {
-    'text': "Learn how to create databases",
-    'done': false,
-    'id': 7
-  },
-  {
-    'text': "Learn SQL",
-    'done': false,
-    'id': 8
-  },
-  {
-    'text': "Learn how to connect to PostgreSQL from Node",
-    'done': false,
-    'id': 9
-  },
-  {
-    'text': "Learn how to use Sequelize",
-    'done': false,
-    'id': 10
-  },
-]
 
 app.post("/", function (req, res) {
   const newestTodo = req.body.newestItem;
